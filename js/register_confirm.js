49956dadd3d7b6681e4894df359501400825c37d
skt-t <co.leveke@duocuc.cl>
2025-09-08 00:00:33 -0300

    Le puse comentarios al js por que se me olvido hacerlo antes...

--- js/register_confirm.js
+++ js/register_confirm.js
@@ -11,14 +11,17 @@ let usuarios = [];
 
 form.addEventListener("submit", (e) => {
     
-    e.preventDefault();
-    if (password.value !== confirmacion.value) {
+    e.preventDefault(); // Esto evita el envio del formulario asi se puede validar que se agregaron correctamente los usuarios
+    
+    // validacion de que las contraseñas coincidan
+    if (password.value !== confirmacion.value) { 
         e.preventDefault();
         alert("¡Las contraseñas no coinciden!");
         confirmacion.focus();
         return;
     }
-
+    
+    // validacion de edad
     if (edad.value < 18) {
         e.preventDefault();
         alert("¡Usted debe ser mayor a 18 años!");
@@ -26,17 +29,21 @@ form.addEventListener("submit", (e) => {
         return;
     }
 
+    // se crea una objeto usuario y se agrega al array de usuarios
     const nuevoUsuario = new Usuario(correo.value, password.value, parseInt(edad.value));
     usuarios.push(nuevoUsuario);
 
+    // de la funcion que esta en usuarios.js se valida si es estudiante o no
     if (nuevoUsuario.esEstudiante()){
         alert("¡Se registro como cuenta de estudiante!\n¡Obtiene un 20% de descuento en todas sus compras!");
     } else {
         alert("¡Registro existoso!");
     }
 
+    // esto resetea el formulario
     form.reset();
 
+    // y bno esto muestra a los usuarios, por eso esta puesto el e.preventDefault() al inicio
     console.log(usuarios);
 
 })
